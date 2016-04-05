9433543f7ca910167924ef9da44a1d7a5f1bfd5d
Edwardgemson <egemson@skidmore.edu>
2016-04-05 13:24:36 -0400

    create sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       if
         user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        flash.notice = "Welcome back to OKLunch #{user.name}"
+        flash.notice = "Welcome back to STFU #{artist.name}"
         redirect_to root_path
       else
         flash[:error] = 'Login failed'
